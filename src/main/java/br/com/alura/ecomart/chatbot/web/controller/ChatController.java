79d24980c5e9ee44d8c3f7fba8b8903e457f8056
Jennyads <jennycristina.1998@gmail.com>
2024-07-07 17:15:12 -0300

    added historic for thread in interface chat

--- src/main/java/br/com/alura/ecomart/chatbot/web/controller/ChatController.java
+++ src/main/java/br/com/alura/ecomart/chatbot/web/controller/ChatController.java
@@ -1,7 +1,9 @@
 package br.com.alura.ecomart.chatbot.web.controller;
 
+import br.com.alura.ecomart.chatbot.domain.service.ChatbotService;
 import br.com.alura.ecomart.chatbot.web.dto.PerguntaDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -10,20 +12,31 @@ public class ChatController {
 
     private static final String PAGINA_CHAT = "chat";
 
+    private ChatbotService service;
+
+    public ChatController(ChatbotService service) {
+        this.service = service;
+    }
+
     @GetMapping
-    public String carregarPaginaChatbot() {
+    public String carregarPaginaChatbot(Model model) {
+        var mensagens = service.carregarHistorico();
+
+        model.addAttribute("historico", mensagens);
+
         return PAGINA_CHAT;
     }
 
     @PostMapping
     @ResponseBody
     public String responderPergunta(@RequestBody PerguntaDto dto) {
-        return dto.pergunta();
+        return service.responderPergunta(dto.pergunta());
     }
 
     @GetMapping("limpar")
     public String limparConversa() {
-        return PAGINA_CHAT;
+        service.limparHistorico();
+        return "redirect:/chat";
     }
 
 }
